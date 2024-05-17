83ed0fdc8feef7cab549e0953bfee8f851ae2647
take.goto.0120@gmail.com <take.goto.0120@gmail.com>
2024-05-18 00:02:52 +0900

    design post view

--- client/src/pages/post/[postId].jsx
+++ client/src/pages/post/[postId].jsx
@@ -4,12 +4,12 @@ import { useRouter } from 'next/router';
 import { useAuth } from '@/hooks/auth';
 import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
-
+// import { audioPlay } from '@mui/icons-material/PlayCircleFilledWhite';
 
 
 // POINT input要素、textarea要素の使い方
 const PostDetail = () => {
-  const { user } = useAuth({ middleware: 'auth' })
+  const { user } = useAuth({ middleware: 'auth'})
 
   //about quiz state
   const [replies, setReplies] = useState([]);
@@ -92,7 +92,7 @@ const PostDetail = () => {
   
   return (
 
-    <AppLayout sx={{textAlign: 'center'}}
+    <AppLayout sx={{textAlign: 'center'} }
         header={
           <h3>
                 詳細表示
@@ -101,38 +101,59 @@ const PostDetail = () => {
         <Head>
             <title>Laravel - 詳細</title>
         </Head>
-      
-      <div>
-        <h3>問題概要</h3>
-        <p>{post.explanation}</p>
-        {/* 後で書き換える */}
-        <button>再生</button>
-      </div>
 
-      <div class="post-detail">
-        <div class="tag border-gray-400 w-2/3 border-solid border-2 rounded p-3 m-2">          
-          <p>リプライ投稿<br />
-          <textarea cols="20" rows="2" onChange={(e) => setText(e.target.value)} value={text}/></p>
+
+        <div class="flex flex-row overflow-hidden">
+          <div class="basis-2/5">
+
+            <div class="flex flex-col">
+
+              <div className='mt-8 ml-20 h-screen'>
+                <h3 className='ml-4'>投稿内容</h3>
+                <div class="tag border-gray-400 w-5/6 h-1/4 border-solid border-2 rounded p-3 m-2 lex flex-col">   
+                  <p class="h-5/6">{post.explanation}</p>
+                {/* 後で書き換える */}
+                  <div class="mb-0 text-right mr-1">
+                    {/* <div><audioPlay className="h-5 w-5"/></div>  */}
+                    <button>再生する</button>
+                  </div>
+                </div>
+              </div>
+
+              <div class="post-detail ml-24 mt-8 absolute bottom-8">
+                <p>リプライ投稿<br/>
+                  <textarea cols="40" rows="4" onChange={(e) => setText(e.target.value)} value={text} border-gray-400/>
+                </p>
+                <div class="text-right mr-4">
+                  <button onClick={() => postReply()}>投稿する</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="basis-3/5 ml-12">
+            <h3 class="mt-8 text-center mr-12">リプライ一覧</h3>
+            <div class="bg-[url('/images/comment.png')] mt-4 w-5/6 h-screen bg-contain bg-no-repeat">
+              
+              
+            <div className='absolute top-60 right-36 h-2/3 w-1/2 overflow-y-scroll flex flex-col-reverse'>
+              {replies.map((reply, index) => (
+                <>
+                  <div key={reply.id}>
+                    <p class="ml-36 pt-8 underline">{reply.user.name}</p>
+                    <p class="ml-36">{reply.text}</p>
+                    <div class="flex flex-row-reverse">
+                      {user.id === reply.user.id && <button class="pr-6">編集</button>}
+                      {user.id === reply.user.id && <button class="pr-3">削除</button>}
+                    </div>
+                  </div> 
+                </>
+              ))}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      
-      <button onClick={() => postReply()}>問題を追加</button>
 
-      <div>
-        <h3>リプライ一覧</h3>
-        
-        {replies.map((reply, index) => (
-          <>
-            <div key={reply.id}>
-              <p>{reply.user.name}:{reply.text}</p>
-              {user.id === reply.user.id && <button>削除</button>}
-              {user.id === reply.user.id && <button>編集</button>}
-            </div> 
-          </>
-        ))}
-
-
-      </div>
     </AppLayout>
   );
 };
